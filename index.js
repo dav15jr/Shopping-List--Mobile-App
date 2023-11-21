6a85e770fb0fc6bc7b7fe8ff5692ef1fffd773c6
dav15jr <dnwanderajroutlook.com>
2023-11-21 20:36:53 +0100

    color changes to button

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://playground-d0610-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://testbed-a58bf-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
